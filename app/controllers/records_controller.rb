219b8a6c9c17246d1602752f59b9893ba396e80e
Emilio Wuerges <wuerges@gmail.com>
2019-03-17 14:35:02 -0300

    missing create action

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,45 +1,22 @@
 class RecordsController < ApplicationController
-  before_action :set_record, only: [:show, :edit, :update, :destroy]
+  before_action :set_record, only: [:update, :destroy]
 
   # GET /records
-  # GET /records.json
   def index
     @records = Record.all
   end
 
-  # GET /records/1
-  # GET /records/1.json
-  def show
-  end
-
-  # GET /records/new
-  def new
-    @record = Record.new
-  end
-
-  # GET /records/1/edit
-  def edit
-  end
-
   # POST /records
-  # POST /records.json
   def create
     @record = Record.new(record_params)
-
     respond_to do |format|
       if @record.save
-        format.html { redirect_to @record, notice: 'Record was successfully created.' }
-        format.json { render :show, status: :created, location: @record }
         format.js
-      else
-        format.html { render :new }
-        format.json { render json: @record.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /records/1
-  # PATCH/PUT /records/1.json
   def update
     respond_to do |format|
       format.js {
@@ -53,7 +30,6 @@ class RecordsController < ApplicationController
   end
 
   # DELETE /records/1
-  # DELETE /records/1.json
   def destroy
     @record.destroy
     respond_to do |format|
