c756015ed362965a9cdd32e6773cb1f9ef3651f2
Emilio Wuerges <wuerges@gmail.com>
2019-03-18 11:02:36 -0300

    falta navbar

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,8 @@
 class Record < ApplicationRecord
   validates_presence_of :weight
   validates_format_of :weight, :with => /\A\d{0,3}(\.\d)?\z/
+
+  def date
+    created_at.strftime("%F")
+  end
 end
